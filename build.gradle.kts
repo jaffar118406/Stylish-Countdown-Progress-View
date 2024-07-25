9d0dfd64a5a4eef4e4f1e3e4042344adee02ea34
jaffar118406 <a33329926@gmail.com>
2024-07-25 12:56:53 +0500

    Added library support

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,18 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    id("maven-publish")
+}
+
+afterEvaluate {
+    publishing{
+        publications{
+            create<MavenPublication>("maven") {
+                from (components.findByName("release"))
+                groupId = "com.github.jaffar118406"
+                artifactId = "countdown-progress-view"
+                version = "0.1.0"
+            }
+        }
+    }
 }
